04cdd79b817c92d34d54c18985e3f4c2711db1ba
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-21 04:21:47 -0400

    initial friends added & images added

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+const friendsArray = [
+  {
+    name   : "Ahmed",
+    photo  : "../public/assets/ahmed.jpg",
+    scores : [ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
+  },
+  {
+    name   : "Carl",
+    photo  : "../public/assets/carl.jpg",
+    scores : [ 4, 2, 3, 5, 4, 2, 1, 4, 5, 2 ]
+  },
+  {
+    name   : "Susan",
+    photo  : "../public/assets/susan.jpg",
+    scores : [ 4, 3, 2, 3, 3, 5, 4, 1, 1, 1 ]
+  },
+  {
+    name   : "David",
+    photo  : "../public/assets/david.jpg",
+    scores : [ 3, 3, 1, 1, 1, 1, 2, 5, 4, 1 ]
+  },
+  {
+    name   : "Katy",
+    photo  : "../public/assets/katy.jpg",
+    scores : [ 5, 5, 4, 4, 5, 5, 5, 5, 4, 5 ]
+  }
+];
