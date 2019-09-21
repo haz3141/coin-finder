f34cb51d76035ad6fa9f66590465f5bd7e5a0863
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-21 05:03:12 -0400

    friend matching logic complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 const friendsArray = [
   {
     name   : "Ahmed",
-    photo  : "../public/assets/ahmed.jpg",
+    photo  : "../public/assets/images/ahmed.jpg",
     scores : [ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
   },
   {
     name   : "Carl",
-    photo  : "../public/assets/carl.jpg",
+    photo  : "../public/assets/images/carl.jpg",
     scores : [ 4, 2, 3, 5, 4, 2, 1, 4, 5, 2 ]
   },
   {
     name   : "Susan",
-    photo  : "../public/assets/susan.jpg",
+    photo  : "../public/assets/images/susan.jpg",
     scores : [ 4, 3, 2, 3, 3, 5, 4, 1, 1, 1 ]
   },
   {
     name   : "David",
-    photo  : "../public/assets/david.jpg",
+    photo  : "../public/assets/images/david.jpg",
     scores : [ 3, 3, 1, 1, 1, 1, 2, 5, 4, 1 ]
   },
   {
     name   : "Katy",
-    photo  : "../public/assets/katy.jpg",
+    photo  : "../public/assets/images/katy.jpg",
     scores : [ 5, 5, 4, 4, 5, 5, 5, 5, 4, 5 ]
   }
 ];
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,17 +1,57 @@
 // DEPENDENCIES
 // ===============================================================================
-const coinData = require("../data/coins");
+const friends = require("../data/friends");
 
 // ROUTES
 // ===============================================================================
 module.exports = function(app) {
-  // GET COINS
-  app.get("/api/coins", function(req, res) {
-    res.json(coinData);
+  // GET FRIENDS
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
   });
 
-  // POST COINS
-  app.post("/api/coins", function(req, res) {
-    console.log("POST COINS");
+  // POST FRIENDS
+  app.post("/api/friends", function(req, res) {
+    // ADD NEW FRIEND
+    const newFriend = req.body;
+    friends.push(newFriend);
+
+    // SAVE MATCHING FRIEND
+    let match = {
+      name  : "",
+      photo : ""
+    };
+
+    // SAVE BEST SCORE
+    let tempMatch = 999;
+
+    // TRAVERSE FRIENDS ARRAY
+    for (let i = 0; i < friends.length; i++) {
+      // ARRAY OF SCORE DIFFERENCES
+      const differenceArray = [];
+      let totalDifference = 0;
+
+      // TRAVERSE SCORES & SAVE DIFFERENCES
+      for (let j = 0; j < 10; j++) {
+        let difference = Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+        differenceArray.push(difference);
+      }
+
+      // ADD DIFFERENCES
+      for (let k = 0; i < differenceArray.length; i++) {
+        totalDifference += differenceArray[k];
+      }
+
+      // COMPARE DIFFERENCES & SET LOWEST MATCH
+      if (totalDifference <= tempMatch) {
+        match = {
+          name  : friends[i].name,
+          photo : friends[i].photo
+        };
+      }
+    }
+
+    // EXIT LOOP & RETURN MATCH
+    res.send(match);
   });
 };
